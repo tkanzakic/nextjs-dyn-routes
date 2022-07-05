ac60a7d0bd5367e784ae7fc1b81c18b57ca835d7
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-07-05 10:06:44 +0200

    feat: add composed dynamic route

--- src/navbar.js
+++ src/navbar.js
@@ -2,11 +2,28 @@ import Link from 'next/link';
 
 export const NavBar = () => (
   <nav>
-    <Link href='/static-1'>Static-1</Link>
-    <Link href='/static-2'>Static-2</Link>
-    <Link href='/static-3'>Static-3</Link>
-    <Link href='/dynamic-1'>dynamic-1</Link>
-    <Link href='/dynamic-2'>dynamic-2</Link>
-    <Link href='/dynamic-3'>dynamic-3</Link>
+    <ul>
+      <li>
+        <Link href='/static-1'>Static-1</Link>
+      </li>
+      <li>
+        <Link href='/static-2'>Static-2</Link>
+      </li>
+      <li>
+        <Link href='/static-3'>Static-3</Link>
+      </li>
+      <li>
+        <Link href='/dynamic-1'>dynamic-1</Link>
+      </li>
+      <li>
+        <Link href='/dynamic-2'>dynamic-2</Link>
+      </li>
+      <li>
+        <Link href='/dynamic-3'>dynamic-3</Link>
+      </li>
+      <li>
+        <Link href='/long/dynamic'>long/dynamic</Link>
+      </li>
+    </ul>
   </nav>
 );
