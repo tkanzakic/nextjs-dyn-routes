40e1b44a58a084e94f29b08286f631f105d8a213
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-07-05 10:12:06 +0200

    feat: mark if route works or not

--- src/navbar.js
+++ src/navbar.js
@@ -4,25 +4,28 @@ export const NavBar = () => (
   <nav>
     <ul>
       <li>
-        <Link href='/static-1'>Static-1</Link>
+        <Link href='/'>Home page</Link> ✅
       </li>
       <li>
-        <Link href='/static-2'>Static-2</Link>
+        <Link href='/static-1'>Static-1</Link> ✅
       </li>
       <li>
-        <Link href='/static-3'>Static-3</Link>
+        <Link href='/static-2'>Static-2</Link> ✅
       </li>
       <li>
-        <Link href='/dynamic-1'>dynamic-1</Link>
+        <Link href='/static-3'>Static-3</Link> ✅
       </li>
       <li>
-        <Link href='/dynamic-2'>dynamic-2</Link>
+        <Link href='/dynamic-1'>dynamic-1</Link> ❌
       </li>
       <li>
-        <Link href='/dynamic-3'>dynamic-3</Link>
+        <Link href='/dynamic-2'>dynamic-2</Link> ❌
       </li>
       <li>
-        <Link href='/long/dynamic'>long/dynamic</Link>
+        <Link href='/dynamic-3'>dynamic-3</Link> ❌
+      </li>
+      <li>
+        <Link href='/long/dynamic'>long/dynamic</Link> ✅
       </li>
     </ul>
   </nav>
